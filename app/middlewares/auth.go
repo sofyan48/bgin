6468a4b603a069615f82d521c9066464f63a916c
Sofyan Saputra <meongbego@gmail.com>
2019-10-06 03:55:35 +0700

    Add: Auth Token function

--- app/middlewares/auth.go
+++ app/middlewares/auth.go
@@ -2,23 +2,25 @@ package middlewares
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"strings"
-	"net"
+
 	"github.com/gin-gonic/gin"
 	"github.com/meongbego/bgin/app/helper"
 	"github.com/meongbego/bgin/app/libs"
+	redis "github.com/meongbego/bgin/app/moduls/package"
 )
 
 func ChekcIPRange(w_ip []string, addr string) bool {
-	
-	for _,ip := range w_ip {
+
+	for _, ip := range w_ip {
 		ip_fix := strings.ReplaceAll(ip, " ", "")
 		_, ipv4Net, err := net.ParseCIDR(ip_fix)
 		if err != nil {
 			fmt.Println(err)
 		}
-		if ipv4Net.Contains(net.ParseIP(addr)){
+		if ipv4Net.Contains(net.ParseIP(addr)) {
 			return true
 			break
 		}
@@ -26,18 +28,18 @@ func ChekcIPRange(w_ip []string, addr string) bool {
 	return false
 }
 
-func CheckValidIP(c *gin.Context,ip string) {
+func CheckValidIP(c *gin.Context, ip string) {
 	whitelist_addr := libs.GetEnvVariabel("ACL_ADDR", os.Getenv("ACL_ADDR"))
-	cidr := strings.Split(whitelist_addr,",")
-	
-	if (ip == "::1"){
+	cidr := strings.Split(whitelist_addr, ",")
+
+	if ip == "::1" {
 		c.Next()
-	} else{
+	} else {
 		check := ChekcIPRange(cidr, ip)
-		if (check != true){
+		if check != true {
 			helper.ResponseMsg(c, 401, "Not Valid IP")
 			c.Abort()
-		}else{
+		} else {
 			c.Next()
 		}
 	}
@@ -47,5 +49,14 @@ func AuthACL() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ip := c.ClientIP()
 		CheckValidIP(c, ip)
+	}
+}
+
+func AuthToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header["Access-Token"]
+		store := redis.Store
+		data := "DATA"
+		store.Do("SET", data, token)
 	}
 }
